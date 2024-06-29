9f0a298254c40f8e306461e135a61757666ceca1
dungbc-0212 <bui.cong.dung@sun-asterisk.com>
2024-06-29 10:44:57 +0700

    initialized

---
+++ server.js
@@ -0,0 +1,22 @@
+const { Server } = require("socket.io");
+const http = require("http");
+
+const server = http.createServer();
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("message", (msg) => {
+    io.emit("message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
