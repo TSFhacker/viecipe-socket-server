2c54afd48371883ea5f1da108ed8100cb3ce8327
dungbc-0212 <bui.cong.dung@sun-asterisk.com>
2024-06-29 11:04:02 +0700

    fix cors 2

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
+    allowedHeaders: ["Authorization"],
+    credentials: true,
   },
 });
 
