69b8dabe235f3bc61282446ab0144e04b19c34b9
dungbc-0212 <bui.cong.dung@sun-asterisk.com>
2024-06-29 14:46:26 +0700

    fix

--- server.js
+++ server.js
@@ -1,21 +1,62 @@
+const { MongoClient } = require("mongodb");
+
+// Replace with your MongoDB connection string
+const uri =
+  "mongodb+srv://dungbui1110:dragonnica2001@cluster0.hm6gngk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; // Example: 'mongodb://username:password@host:port/databaseName'
+
+let db;
+
+async function connectToDatabase() {
+  try {
+    const client = await MongoClient.connect(uri);
+    console.log("Connected to MongoDB");
+
+    // Specify your database name
+    db = client.db(); // Replace with your database name
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+// Call connectToDatabase to establish connection
+connectToDatabase();
+
 const { Server } = require("socket.io");
 const http = require("http");
 
 const server = http.createServer();
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Authorization"],
-    credentials: true,
-  },
-});
+const io = new Server(server);
 
 io.on("connection", (socket) => {
   console.log("a user connected");
 
-  socket.on("message", (msg) => {
-    io.emit("message", msg);
+  // Example: Joining a room based on user ID
+  socket.on("joinRoom", (userId) => {
+    socket.join(userId); // Join room identified by userId
+    console.log(`User ${userId} joined the room`);
+  });
+
+  // Example: Handling private messages
+  socket.on("privateMessage", async ({ senderId, recipientId, input }) => {
+    try {
+      const messagesCollection = db.collection("messages");
+      const result = await messagesCollection.insertOne({
+        senderId,
+        recipientId,
+        input,
+        timestamp: new Date(),
+        status: "unread",
+      });
+
+      console.log("Message stored in MongoDB:", result.insertedId);
+
+      // Emit message to a specific room (user)
+      console.log(input, senderId, recipientId);
+      io.to(recipientId).emit("privateMessage", input);
+      io.emit("message", input);
+    } catch (error) {
+      console.error("Error storing message in MongoDB:", error);
+    }
   });
 
   socket.on("disconnect", () => {
@@ -23,7 +64,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
